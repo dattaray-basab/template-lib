c6c0476d61894dd1ae68de1c77efcf64dc98a21c
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-07-13 20:26:06 -0700

    0.0.5: setenv - implementing GetNamedPath

--- mgr/misc/envmgr.go
+++ mgr/misc/envmgr.go
@@ -2,15 +2,15 @@ package misc
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/dattaray-basab/template-lib/mgr/globals"
+		"github.com/dattaray-basab/template-lib/common"
 )
 
 func SetRecipePath(dirPath string) {
 	fmt.Println("misc/envmgr.go::SetRecipePath: path::", dirPath)
 	fmt.Println("misc/envmgr.go::SetRecipePath: globals.RecipePathKey::", globals.RecipePathKey)
-	foundPath, err := GetNamedPath(dirPath, "___DEV_3")
+	foundPath, err := common.GetNamedPath(dirPath, "__DEV_3")
 	if err != nil {
 		fmt.Println("misc/envmgr.go::SetRecipePath: err::", err)
 		return 
@@ -18,15 +18,3 @@ func SetRecipePath(dirPath string) {
 	fmt.Println("misc/envmgr.go::SetRecipePath: foundPath::", foundPath)
 }
 
-func GetNamedPath(dirPath string, searchPathFragment string) (string, error)  {
-	dirPathParts := strings.Split(dirPath, "/")
-	newPath := ""
-	for i, dirPathPart := range dirPathParts {
-		newPath = newPath + dirPathPart + "/"
-		if dirPathPart == searchPathFragment {
-			fmt.Println("misc/envmgr.go::SetRecipePath: index, dirPathPart::", i, dirPathPart)
-			return newPath, nil
-		}
-	}
-	return "", fmt.Errorf("misc/envmgr.go::SetRecipePath: %s not found in %s", searchPathFragment, dirPath)
-}
