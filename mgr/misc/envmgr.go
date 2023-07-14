a3fca765b0dbbae9e696f03ac83bbcd63da404df
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-07-13 20:17:12 -0700

    0.0.5: setenv - implementing GetNamedPath

--- mgr/misc/envmgr.go
+++ mgr/misc/envmgr.go
@@ -10,13 +10,23 @@ import (
 func SetRecipePath(dirPath string) {
 	fmt.Println("misc/envmgr.go::SetRecipePath: path::", dirPath)
 	fmt.Println("misc/envmgr.go::SetRecipePath: globals.RecipePathKey::", globals.RecipePathKey)
+	foundPath, err := GetNamedPath(dirPath, "___DEV_3")
+	if err != nil {
+		fmt.Println("misc/envmgr.go::SetRecipePath: err::", err)
+		return 
+	}
+	fmt.Println("misc/envmgr.go::SetRecipePath: foundPath::", foundPath)
+}
 
-	// split a string into a slice of strings
+func GetNamedPath(dirPath string, searchPathFragment string) (string, error)  {
 	dirPathParts := strings.Split(dirPath, "/")
+	newPath := ""
 	for i, dirPathPart := range dirPathParts {
-		if dirPathPart == globals.RecipeDirName {
+		newPath = newPath + dirPathPart + "/"
+		if dirPathPart == searchPathFragment {
 			fmt.Println("misc/envmgr.go::SetRecipePath: index, dirPathPart::", i, dirPathPart)
+			return newPath, nil
 		}
 	}
-
+	return "", fmt.Errorf("misc/envmgr.go::SetRecipePath: %s not found in %s", searchPathFragment, dirPath)
 }
