628d2b88fe6930e59619e674e4d60c779dd32db2
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-07-13 19:57:27 -0700

    0.0.4: ? setenv

--- mgr/globals/constants.go
+++ mgr/globals/constants.go
@@ -1,3 +1,5 @@
 package globals
 
 const RecipePathKey = "CKS_ENV_RECIPE_PATH"
+// const RecipeDirName = "__recipe"
+const RecipeDirName = "__DEV_3"
--- mgr/misc/envmgr.go
+++ mgr/misc/envmgr.go
@@ -2,10 +2,21 @@ package misc
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/dattaray-basab/template-lib/mgr/globals"
 )
 
-func SetRecipePath(path string) {
-	fmt.Println("misc/envmgr.go::SetRecipePath: path::", path)
+func SetRecipePath(dirPath string) {
+	fmt.Println("misc/envmgr.go::SetRecipePath: path::", dirPath)
 	fmt.Println("misc/envmgr.go::SetRecipePath: globals.RecipePathKey::", globals.RecipePathKey)
+
+	// split a string into a slice of strings
+	dirPathParts := strings.Split(dirPath, "/")
+	for i, dirPathPart := range dirPathParts {
+		if dirPathPart == globals.RecipeDirName {
+			fmt.Println("misc/envmgr.go::SetRecipePath: index, dirPathPart::", i, dirPathPart)
+		}
+	}
+
 }
