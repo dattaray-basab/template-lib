a85d61825c313f6c0dbaf1f2ff7ec141703caadd
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-07-15 14:39:16 -0700

    0.0.6: added phase

--- mgr/phase.go
+++ mgr/phase.go
@@ -2,17 +2,21 @@ package mgr
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/dattaray-basab/template-lib/common"
 )
 
 func NewPhase(
-	absPathToRecipe string,
+	templaterDirpath string,
 	phaseName string,
 	codeBlock string,
 	dependentPhaseNames string) {
 		
-		fmt.Println("NewRecipe")
-		recipeDirName := common.GetRecipePath(absPathToRecipe)
+		fmt.Println("NewPhase")
+		recipeDirName := common.GetRecipePath(templaterDirpath)
+		if templaterDirpath != recipeDirName || !strings.HasPrefix(templaterDirpath, recipeDirName) {
+			_ = fmt.Errorf("Recipe path %s is not a subdirectory of %s", templaterDirpath, recipeDirName)
+		}
 		fmt.Println("recipeDirName::", recipeDirName)
 }
