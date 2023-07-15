d1fc6009f02f1d5d9c92a24fb6aef3ea84b3dfe2
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-07-15 14:48:21 -0700

    0.0.6: added phase

--- mgr/phase.go
+++ mgr/phase.go
@@ -11,12 +11,12 @@ func NewPhase(
 	templaterDirpath string,
 	phaseName string,
 	codeBlock string,
-	dependentPhaseNames string) {
-		
+	dependentPhaseNames string) {	
 		fmt.Println("NewPhase")
 		recipeDirName := common.GetRecipePath(templaterDirpath)
-		if templaterDirpath != recipeDirName || !strings.HasPrefix(templaterDirpath, recipeDirName) {
+		fmt.Println("templaterDirpath::", templaterDirpath)
+		fmt.Println("recipeDirName::", recipeDirName)
+		if templaterDirpath != recipeDirName && !strings.HasPrefix(templaterDirpath, recipeDirName) {
 			_ = fmt.Errorf("Recipe path %s is not a subdirectory of %s", templaterDirpath, recipeDirName)
 		}
-		fmt.Println("recipeDirName::", recipeDirName)
 }
